7c1dff34b9931cd0cd739213c3806bd0ab8913aa
Monchkrit <blitzkritter@gmail.com>
2019-03-09 00:45:13 +0000

    There is some security holding unregistered students from viewing lesson content.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson, only: [:show]
 
 	def show
 
@@ -10,4 +12,10 @@ class LessonsController < ApplicationController
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+  def require_authorized_for_current_lesson
+    if current_lesson.section.course.user != current_user
+      redirect_to root_path, alert: 'You are not currently enrolled in this the course. Please enroll to gain access to the lesson content.'
+    end
+  end
 end
