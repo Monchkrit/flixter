0dc3caa83eec61d2a00eef5d4214a03f9d5ec058
Monchkrit <blitzkritter@gmail.com>
2019-03-16 03:13:21 +0000

    I can not add lessons through the JQuery methods.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -5,3 +5,8 @@ class Lesson < ApplicationRecord
   include RankedModel
   ranks :row_order, with_same: :section_id
 end
+
+#def next_lesson
+#  lesson = section.lessons.where("row_order > ?", self.row_order).rank(:row_order).first
+#  return lesson
+#end
